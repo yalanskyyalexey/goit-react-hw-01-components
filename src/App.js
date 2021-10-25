2013edcb794834cd41c81d2a5c344b8c4de3cfdd
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-10-26 00:05:26 +0300

    add hw1 part 1 and 2

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
+// import FriendList from './components/FriendList/FriendList';
+// import Transaction from './components/Transaction/Transaction';
+
+import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
+// import friends from './data/friend.json';
+// import transaction from './data/transaction.json';
+
+const App = () => {
+	return (
+		<>
+			<Profile
+				name={user.name}
+				tag={user.tag}
+				location={user.location}
+				avatar={user.avatar}
+				stats={user.stats}
+			/>
+			<Statistics title='Upload stats' stats={statisticalData} />
+		</>
+	);
+};
+
+export default App;
---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Profile.module.css';
+
+const Profile = ({ name, avatar, tag, location, stats }) => {
+	return (
+		<div className={styles.profile}>
+			<div className={styles.description}>
+				<img src={avatar} alt={name} className={styles.avatar} />
+				<p className={styles.name}>{name}</p>
+				<p className={styles.tag}>@{tag}</p>
+				<p className={styles.location}>{location}</p>
+			</div>
+			<ul className={styles.stats}>
+				<li>
+					<span className={styles.label}>Followers</span>
+					<span className={styles.quantity}>{stats.followers}</span>
+				</li>
+				<li>
+					<span className={styles.label}>Views</span>
+					<span className={styles.quantity}>{stats.views}</span>
+				</li>
+				<li>
+					<span className={styles.label}>Likes</span>
+					<span className={styles.quantity}>{stats.likes}</span>
+				</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Profile;
+
+Profile.propTypes = {
+	name: PropTypes.string.isRequired,
+	avatar: PropTypes.string.isRequired,
+	tag: PropTypes.string.isRequired,
+	location: PropTypes.string.isRequired,
+	stats: PropTypes.shape({
+		followers: PropTypes.number.isRequired,
+		views: PropTypes.number.isRequired,
+		likes: PropTypes.number.isRequired,
+	}).isRequired,
+};
